fa4005820fa2b22f196f425f1d57d49629860fc1
shiftux <shiftux@gmail.com>
2020-09-22 16:32:11 +0200

    form working, missing captcha

---
+++ mailer.js
@@ -0,0 +1,50 @@
+const nodemailer = require('nodemailer');
+const { resolveContent } = require('nodemailer/lib/shared');
+
+class Mailer {
+  constructor(email, message, name='', company=null, sendCV=false) {
+    this.name = name;
+    this.email = email;
+    this.message = message;
+    this.company = company;
+    this.sendCV = sendCV;
+    this.transporter = nodemailer.createTransport({
+      service: "Mailgun",
+      auth: {
+        user: process.env.MAILER_USER,
+        pass: process.env.MAILER_PW
+      }
+    });
+    this.emailOptions = {
+      from: process.env.MAILER_MYMAIL,
+      to: 's.montanari@gmx.ch', // to: process.env.MAILER_MYMAIL,
+      subject: 'Received message on Website',
+      text: this.message
+    };
+  }
+  sendMeTheMessage() {
+    const msg = `Sender: ${this.name}\nCompany: ${this.company}\nWants CV: ${this.sendCV}\nMessage: ${this.message}`
+    this.emailOptions['text'] = msg;
+    return this.transporter.sendMail(this.emailOptions)
+  }
+  confirmInquiry(){
+    this.emailOptions['subject'] = 'Contacted Sandro Montanari';
+    let msg = `Hi ${this.name},\n\n Thanks for reaching out! I've received your inquiry and will be in touch soon.\n`;
+    const copy = `PS Below you'll find a copy of your inquiry:\n\n${this.message}\n\n`
+    const closing =  'Best regards, Sandro\n\n';
+    if(this.sendCV){
+      msg = msg.concat('Please find the requested CV attached.\n\n');
+      this.emailOptions['attachments'] = [
+        {
+          filename: 'SandroMontanari_CV.pdf',
+          path: 'assets/CV.pdf'
+        },
+      ]
+    }
+    msg = msg.concat(closing, copy);
+    this.emailOptions['text'] = msg;
+    return this.transporter.sendMail(this.emailOptions)
+  }
+}
+
+module.exports.Mailer = Mailer
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
+require('../mailer.js');
 var express = require('express');
+const { Mailer } = require('../mailer.js');
 var router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/connectForm', [
+      body('inputEmail').isEmail(),
+    ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log('ERROR: ', errors);
+      res.redirect('/mail_error.html');
+    }
+    const mailer = new Mailer(
+      req.body.inputEmail,
+      req.body.messageField,
+      req.body.inputName,
+      req.body.inputCompany,
+      req.body.cvCheckbox
+    )
+    const prom1 = mailer.sendMeTheMessage();
+    const prom2 = mailer.confirmInquiry();
+    Promise.all([prom1, prom2]).then((values) => {
+      res.redirect('/mail_thanks.html');
+    }).catch((error) => {
+      console.log('ERROR: ', error);
+      res.redirect('/mail_error.html');
+    });
 });
 
 module.exports = router;
